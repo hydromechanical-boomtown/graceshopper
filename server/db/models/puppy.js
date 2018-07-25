c09376f006f461539c35bcd3297366e846309632
Stephen Ko <prodigious13@gmail.com>
2018-07-25 19:08:13 -0400

    bug fix for Puppy model and incorporated seed info from seed branch #28

--- server/db/models/puppy.js
+++ server/db/models/puppy.js
@@ -1,43 +1,41 @@
-const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Puppy = db.define('puppy', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-    price: {
-      type: Sequelize.INTEGER,
-      allowNull: false
-    },
-    age:{
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    breed: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-    gender:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            isIn: [["Male", "Female"]],
-            msg: "Choose Gender"
-        }   
-    },
-
-    imageURL: {
-      type: Sequelize.STRING,
-      allowNull: false
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  breed: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  gender: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: {
+        args: [['Male', 'Female']],
+        msg: 'Choose Gender'
+      }
     }
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
 
-  })
-  
-  module.exports = Puppy
-
+module.exports = Puppy
