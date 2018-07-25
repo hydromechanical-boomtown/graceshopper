7484227a6300681887455de5a2a6c15eac4805e0
Victor Ravier <victor.ravier@live.fr>
2018-07-25 15:55:12 -0400

    Created Models (#18)
    * changes
    
    * Models Ready
    
    * changes

---
+++ server/db/models/puppy.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+
+const Puppy = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+    price: {
+      type: Sequelize.NUMBER,
+      allowNull: false
+    },
+    age:{
+        type: Sequelize.NUMBER,
+        allowNull: false
+    },
+    breed: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+    gender:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isIn: [["Male", "Female"]],
+            msg: "Choose Gender"
+        }   
+    },
+   
+
+    imageURL: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+
+  })
+  
+  module.exports = Puppy
