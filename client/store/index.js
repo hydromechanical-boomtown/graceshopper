f86367053dea6dd52b6f2fa0c7879b472c321144
Joe Andaluz <jandaluz1@gmail.com>
2018-07-25 15:50:02 -0400

    Puppy reducer (#19)
    * puppy reducer
    
    * ready to test puppy reducer
    
    * test passed
    
    * puppy reducer added to combine reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import puppies from './puppy'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, puppies})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
