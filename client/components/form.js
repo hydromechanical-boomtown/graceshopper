e35082a7cc135c402c71377c854398471663551b
Joe Andaluz <jandaluz1@gmail.com>
2018-07-30 16:31:34 -0400

    added handleChange and handleSumbit back to on form

--- client/components/form.js
+++ client/components/form.js
@@ -2,65 +2,75 @@ import React, {Component} from 'react'
 import Button from '@material-ui/core/Button'
 import {connect} from 'react-redux'
 import TextField from '@material-ui/core/TextField'
-import StripeCheckout from 'react-stripe-checkout';
+import StripeCheckout from 'react-stripe-checkout'
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+  }
 
   render() {
-   return (
-    
-    <div>
-           <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-        <TextField
-          required
-          id="required"
-          label="Required"
-          defaultValue="First Name"
-          margin="normal"
-          onChange={this.handleChange}
-          name="fname"
-        />
-        <TextField
-          required
-          id="required"
-          label="Required"
-          defaultValue="Last Name"
-          margin="normal"
-          onChange={this.handleChange}
-          name="lname"
-     />
-       {this.props.user && (
+    return (
+      <div>
+        <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
           <TextField
             required
             id="required"
             label="Required"
-            defaultValue="Email"
+            defaultValue="First Name"
             margin="normal"
             onChange={this.handleChange}
-            name="email"
-          /> )}
-         <TextField
-          required
-          id="required"
-          label="Required"
-          defaultValue="Address"
-          margin="normal"
-          onChange={this.handleChange}
-          name="address"
-        />
- </form>
- 
-  <StripeCheckout
-  token={this.onToken}
-  stripeKey="pk_test_cBSjAsw49UTK7TvSOl2zpeYu"
-  amount = {this.props.total*100}
-/>
-</div>
+            name="fname"
+          />
+          <TextField
+            required
+            id="required"
+            label="Required"
+            defaultValue="Last Name"
+            margin="normal"
+            onChange={this.handleChange}
+            name="lname"
+          />
+          {this.props.user && (
+            <TextField
+              required
+              id="required"
+              label="Required"
+              defaultValue="Email"
+              margin="normal"
+              onChange={this.handleChange}
+              name="email"
+            />
+          )}
+          <TextField
+            required
+            id="required"
+            label="Required"
+            defaultValue="Address"
+            margin="normal"
+            onChange={this.handleChange}
+            name="address"
+          />
+        </form>
 
-   )
-   
+        <StripeCheckout
+          token={this.onToken}
+          stripeKey="pk_test_cBSjAsw49UTK7TvSOl2zpeYu"
+          amount={this.props.total * 100}
+        />
+      </div>
+    )
   }
-  
 }
 const mapStateToProps = state => {
   const puppiesInCart = state.cart.map(id => {
@@ -85,6 +95,5 @@ const mapStateToProps = state => {
     user: state.user
   }
 }
-
 
 export const ConnectedForm = connect(mapStateToProps)(Form)
