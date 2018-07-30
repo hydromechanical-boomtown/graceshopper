1f8d57fbb8dfebda6fd0da2c00f872b6841eec4d
Joe Andaluz <jandaluz1@gmail.com>
2018-07-30 11:26:43 -0400

    initial cart api for getting/creating/updating cart (#50)

--- server/api/carts.js
+++ server/api/carts.js
@@ -1,3 +1,57 @@
 const router = require('express').Router()
 const {Cart} = require('../db/models')
+
+//gets the cart for user on the session
+//creates cart if no cart exists
+router.get('/', async (req, res, next) => {
+  try {
+    const user = req.session.passport.user
+    let cart = await Cart.find({
+      where: {userId: user}
+    })
+    if (cart) {
+      res.status(200).json(cart)
+    } else {
+      cart = await Cart.create()
+      cart.setUser(user)
+      res.status(201).json(cart)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/', async (req, res, next) => {
+  try {
+    const user = req.session.passport.user
+    const cart = await Cart.findAll({where: {userId: user}})
+    await cart.destroy()
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/', async (req, res, next) => {
+  try {
+    const user = req.session.passport.user
+    const cart = await Cart.findAll({where: {userId: user}})
+    await cart.update(req.body)
+    res.sendStatus(202)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = req.session.passport.user
+    const cart = await Cart.create()
+    cart.userId = user
+    res.status(201).json(cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
 module.exports = router
