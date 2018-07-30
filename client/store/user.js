8df8cb21f8c37b2d0dedf6553468884b26f9bb0f
Joe Andaluz <jandaluz1@gmail.com>
2018-07-30 18:31:49 -0400

    empty cart on state on logout (#56)
    * empty cart on state on logout
    
    * Update user.js
    
    * Update cart.js

--- client/store/user.js
+++ client/store/user.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import history from '../history'
+import {clearCart} from './cart'
 
 /**
  * ACTION TYPES
@@ -62,6 +63,7 @@ export const logout = () => async dispatch => {
   try {
     await axios.post('/auth/logout')
     dispatch(removeUser())
+    dispatch(clearCart())
     history.push('/login')
   } catch (err) {
     console.error(err)
